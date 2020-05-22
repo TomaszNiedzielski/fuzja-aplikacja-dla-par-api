5af01a455ae237cf5ef2d5db8b609c66f34dae7f
tomasz <tomasz9531@gmail.com>
2020-05-22 17:13:37 +0200

    kurwa

--- app/Notifications/NewMessageNotification.php
+++ app/Notifications/NewMessageNotification.php
@@ -13,10 +13,12 @@ class NewMessageNotification extends Notification implements ShouldQueue
     use Queueable;
 
     public $body;
+    public $user_name;
 
     public function __construct($message)
     {
         $this->body = $message->text;
+        $this->user_name = $message->user->name;
     }
 
     public function via($notifiable)
@@ -29,7 +31,7 @@ class NewMessageNotification extends Notification implements ShouldQueue
         return ExpoMessage::create()
             ->badge(1)
             ->enableSound()
-            ->title("New message!")
+            ->title($this->user_name)
             ->setChannelId('chat-messages')
             //->ttl(60)
             ->body($this->body);
